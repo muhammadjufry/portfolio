4d70d3bd81566d1fe4715ebfb34f2917ba8e2578
MuhammadJufri <sayyidmuhammad878@gmail.com>
2022-10-22 12:39:52 +0700

    Add script gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/style.css';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,18 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+      if(navigator.userAgent.indexOf("Chrome-Lighthouse") == -1) { 
+      
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PHZ9GGG');
+       }
+      `}
+      </Script>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>

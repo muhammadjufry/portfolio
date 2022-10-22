30703b8bf045e102f8bd1a43e46c3aa54dd5fad4
MuhammadJufri <sayyidmuhammad878@gmail.com>
2022-10-22 12:08:53 +0700

    Fix 500 internal error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 type Props = {};
 
@@ -11,21 +10,6 @@ export default function _document({}: Props) {
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
-        />
-        <Script
-          id="google-analytics"
-          dangerouslySetInnerHTML={{
-            __html: `
-            if(navigator.userAgent.indexOf("Chrome-Lighthouse") == -1) { 
-
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PHZ9GGG');
-               };
-  `,
-          }}
         />
       </Head>
       <body id="home">
